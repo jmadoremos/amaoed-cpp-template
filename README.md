50ffb494570ae6db417ebb945d748798ed6005d1
jmadoremos <21135564+jmadoremos@users.noreply.github.com>
2017-03-27 10:48:20 +0800

    Added vscode > c++

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# projects-templates
-A collection of templates for different IDEs
+# Templates
+This repository contains common templates used in different IDEs that does not have a default project structure.
+
+This currently supports:
+* Visual Studio Code
+	- C++
+
+# Notes:
+* Visual Studio Code
+	* C++
+		- MinGW should be located at the root of drive C:
+
+# History Logs:
+* 2017-03-27
+	- Added vscode > c++ support
---
+++ templates/vscode/c++/src/main.cpp
@@ -0,0 +1,36 @@
+/*****************************************************************
+ * @author: <your name here>
+ * @app name: <your application name here>
+ * @app desc: <describe the purpose of your application here>
+ * @history:
+ *  - <yyyy/mm/dd> | <name>
+ *      -- <description>
+ *      -- <description>
+ *      -- <description>
+ *  - <yyyy/mm/dd> | <name>
+ *      -- <description>
+ *      -- <description>
+ *      -- <description>
+ *****************************************************************/
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+// *NOTE*
+// This is your program entry point. Your main logic is placed
+// inside this function. You may add your functions before this
+// "main()", or after this "main()" provided you added reference
+// before this "main()" function.
+int main() {
+    // *TO DO*
+    // Place your code logic here
+    // ...
+
+    // *DO NOT CHANGE*
+    // Print a new line and ask user for any key before exiting
+    cout << endl;
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
