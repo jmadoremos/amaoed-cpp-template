399d9ab9289a749b09c4d22a471bd608baef940f
James Michael Adoremos <adoremos162@gmail.com>
2018-02-08 11:13:59 +0800

    Changed reference from system-specific PAUSE to library-oriented _pause function

--- src/main.cpp
+++ src/main.cpp
@@ -12,30 +12,32 @@
  *      -- <description>
  *      -- <description>
  * ****************************************************************/
+
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
+#include "_pause.h"
 
 using namespace std;
 
 //////////////////////////////////////////////////////////////////
-//                               NOTE 
+//                               NOTE
 // This is your program entry point. Your main logic is placed
 // inside this function. You may add your functions before this
 // "main()", or after this "main()" provided you added reference
 // before this "main()" function.
 //////////////////////////////////////////////////////////////////
+
 int main() {
     // ************************** TO DO **************************
     // Place your code logic after this comment line
     // ***********************************************************
 
-
+    cout << "Hello World!";
 
     // ********************** DO NOT CHANGE **********************
     // Print a new line and ask user for any key before exiting
     // ***********************************************************
-    cout << endl;
-    system("PAUSE");
+    _pause();
     return EXIT_SUCCESS;
 }
